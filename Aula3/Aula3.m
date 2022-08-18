fb268a5cd43f2e9c91e44d8af662abce902f235b
Maria Eduarda Pedroso <a2150336@vmpdc.utfpr>
2022-08-18 14:50:26 -0300

    pt2 exercicio

--- Aula3/Aula3.m
+++ Aula3/Aula3.m
@@ -5,3 +5,11 @@ ct=ilaplace(C);%laplace inversa
 
 t=0:0.01:1;%valores
 plot(t,1/2 - exp(-2*t)/2);
+%%
+syms s
+
+C = 1/(s*(s+4)*(s+8));
+ilaplace(C);
+
+t=0:0.01:1;%valores
+plot(t,exp(-8*t)/32 - exp(-4*t)/16 + 1/32);
