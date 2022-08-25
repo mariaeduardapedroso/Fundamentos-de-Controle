2171cbef6de7dd72268d3643887f193a26a68640
Maria Eduarda Pedroso <a2150336@vmpdc.utfpr>
2022-08-25 15:19:15 -0300

    exercicio 2 da aula usando mat e simu

--- Aula5/aula05matriz.m
+++ Aula5/aula05matriz.m
@@ -1,20 +1,21 @@
+%% EXERCICIO 1
 syms s I1 I2 I3 V
 
-A=[(2*s+2) -(1+2) -1;-(1+2*s) (1+4*s) -4*s;-1 -4*s (1+4*s+1/s)];
+A=[(2*s+2) -(1+2) -1;-(1+2*s) (1+4*s) -4*s;-1 -4*s (1+4*s+1/s)]; %MATRIZ DOS VALORES 
 
-B=[I1; I2; I3];
+B=[I1; I2; I3]; %MATRIZ COM OQ QUEREMOS ACHAR
 
-C=[V;0;0];
+C=[V;0;0]; % MATRIZ RESULTADO
  
-B= inv(A)*C;
+B= inv(A)*C; 
 
 
 pretty(B)
 
-%%
-g=tf([0 8 13 1 0],[24 30 17 16 1]);
-step(g)
-%%
+%% EXERCICIO 1 COM STEP
+g=tf([0 8 13 1 0],[24 30 17 16 1]);%POLINOMIO
+step(g) %DEGRAU NO G
+%% EXERCICIO 2
 syms s I1 I2 I3 V
 
 A=[(1+s) -s -1;-s (2*s+1) -1;-1 -1 (2+s)];
@@ -28,3 +29,6 @@ B= inv(A)*C;
 
 pretty(B)
 
+%% EXERCICIO 2 COM STEP
+g=tf([1 2 1],[1 5 2]);
+step(g)
